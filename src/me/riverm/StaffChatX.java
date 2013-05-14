92bde1834168f738faee9612686287b84723baa2
River Marmorstein <Rivermarmorstein@gmail.com>
2013-05-14 15:00:57 -0400

    update config

--- src/me/riverm/StaffChatX.java
+++ src/me/riverm/StaffChatX.java
@@ -1,16 +1,43 @@
 package me.riverm;
 
+import org.apache.commons.lang.StringUtils;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class StaffChatX extends JavaPlugin {
 	
 	
 	public void onEnable() {
-		getCommand("sc").setExecutor(new sc());
 		this.saveDefaultConfig();
 	}
-	
 	public void onDisable() {
-		
 	}
+	
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if(label.equalsIgnoreCase("sc")) {
+		    if(!(sender instanceof Player)) {
+		        return false;
+		    }
+		    if (args.length == 0 || !(sender.hasPermission("StaffChatX.send"))) {
+		        return false;
+		    } else {
+		    String sperm = "StaffChatX.receive";
+		       
+		    String msg = StringUtils.join(args, " ");
+		    Player play = (Player) sender;
+		    
+		    for(Player p : Bukkit.getServer().getOnlinePlayers()) {
+		    	if (p.hasPermission(sperm)) {
+		    		p.sendMessage(ChatColor.BLUE + this.getConfig().getString("prefix") + ChatColor.WHITE + play.getDisplayName() + "> " + msg);
+		    		}
+		    	}
+		    }
+	    }
+		return false;	
+	}
+
 }
