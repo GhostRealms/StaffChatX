b9551cc3c182ec2d3ba0246591fea61154a4201f
River Marmorstein <Rivermarmorstein@gmail.com>
2013-05-13 22:42:56 -0400

    add default config

--- src/me/riverm/main.java
+++ src/me/riverm/main.java
@@ -5,11 +5,10 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class main extends JavaPlugin {
 	
-	PluginDescriptionFile pdfFile = this.getDescription();
 	
 	public void onEnable() {
-		getCommand("sc").setExecutor(new scexe());
-		getCommand("scinfo").setExecutor(new scinfoexe());
+		getCommand("sc").setExecutor(new sc());
+		this.saveDefaultConfig();
 	}
 	
 	public void onDisable() {
